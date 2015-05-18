323159c5ba9ed0fc651f306221aa5924ba74fffb
yanqingmen <toofooltosay@gmail.com>
2015-05-18 02:11:28 -0700

    small change

--- rnn_toy/node.py
+++ rnn_toy/node.py
@@ -7,13 +7,18 @@ author: hzx
 
 class Node(object):
     '''node class'''
-    def __init__(self, data, info=""):
+    def __init__(self, data, grad=None, info=""):
         self._data = data
+        self._grad = grad
         self._info = info
 
     def get_data(self):
         '''get data'''
         return self._data
+
+    def get_grad(self):
+        '''get gradient'''
+        return self._grad
 
     def get_info(self):
         '''get node info'''
